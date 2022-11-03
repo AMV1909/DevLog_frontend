94d441ec60071ae52619c4395d00aa6abaec6177
Axel <78486180+AMV1909@users.noreply.github.com>
2022-11-03 12:35:54 -0500

    Add files via upload

--- src/api/login.js
+++ src/api/login.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 const login = async () => {
-    return await axios.post("http://localhost:4000/login");
+    return await axios.post("http://157.245.95.77:4000/login");
 }
