64dbcdd743d3f3e6aec62399ed38d8bf8483270d
AMV1909 <amorales264@unab.edu.co>
2022-11-13 13:30:31 -0500

    Estoy haciendo pruebas no más

--- src/api/backend.js
+++ src/api/backend.js
@@ -1,3 +1,5 @@
-export const backend = "http://157.245.95.77:4000";
+//export const backend = "http://157.245.95.77:4000";
 
 //export const backend = "http://localhost:4000";
+
+export const backend = "https://backend-production-18d8.up.railway.app";
