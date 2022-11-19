24f93ea05e494685a7cc37eb263acb58c82eec4d
sbustos497 <sbustos629@unab.edu.co>
2022-11-19 11:16:00 -0500

    Pag pedidos

--- src/pages/Pedidos.jsx
+++ src/pages/Pedidos.jsx
@@ -15,7 +15,30 @@ export const Pedidos = () => {
         <>
             <Navbar />
 
-            <h1 className='text-center'>Aquí van los pediddos</h1>
+          
+
+
+            <div className="container containerPedido">
+                <div className="row ">
+                    {pedidos.map((pedido) => (
+                        <div className="col-12 card mt-5 colPedido ">
+                            <h1 className='text-center'>Pedido</h1>
+                            <hr />
+                            <div>
+                                <h5>Nombre del destinatario: {pedido.name}</h5>
+                                <h5>Correo: {pedido.email}</h5>
+                                <h5>Direccion: {pedido.address}</h5>
+                            </div>
+
+                            <h4 className='totalPedido'>Total: {pedido.total}</h4>
+                            
+                           
+                        </div>
+                    ))}
+                </div>
+            </div>
+                        
+            
         </>
     )
 }
